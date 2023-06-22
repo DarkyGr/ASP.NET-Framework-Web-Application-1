8f0d73759782c0cdc6f82c4f0fd0e1650696cbd9
Guillermo Riaño <guilledark17@gmail.com>
2023-06-22 11:02:00 -0600

    ✅ Adding User & Email Models

---
+++ ASP.NET_Web_App_NetFramework_1/Models/EmailDTO.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace ASP.NET_Web_App_NetFramework_1.Controllers
+{
+    public class EmailDTO
+    {
+        public string From { get; set; }
+        public string Subject { get; set; }
+        public string Content { get; set; }
+    }
+}
---
+++ ASP.NET_Web_App_NetFramework_1/Models/UserDTO.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace ASP.NET_Web_App_NetFramework_1.Models
+{
+    public class UserDTO
+    {
+        public int u_Id { get; set; }
+        public string uName { get; set; }
+        public string uEmail { get; set; }
+        public string uPassword { get; set; }
+        public string passwordConfirmed { get; set; }
+        public bool uReset { get; set;}
+        public bool uConfirmed { get; set; }
+        public string uToken { get; set; }
+    }
+}
